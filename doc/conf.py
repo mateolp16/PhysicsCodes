794e01af1d11737ac648701fe9741097f54f157a
Elizabeth <132688916+eli12gran@users.noreply.github.com>
2023-08-29 15:33:17 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../source'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.mathjax"]
+extensions = ["myst_parser","sphinx.ext.autodoc", "sphinx.ext.mathjax"]
 suppress_warnings = ["myst.header"]
 
 # Add any paths that contain templates here, relative to this directory.
