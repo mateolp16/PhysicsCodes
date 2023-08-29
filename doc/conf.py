1314d4f4bed2afb03f711474a46c6ccc92606d5a
Elizabeth <elizabethgran12@gmail.com>
2023-08-29 14:47:49 -0500

    quite wening

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../source'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.mathjax"]
-#suppress_warnings = ["myst.header"]
+suppress_warnings = ["myst.header"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
